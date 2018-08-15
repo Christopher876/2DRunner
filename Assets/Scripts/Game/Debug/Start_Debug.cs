c70740eed68b3e3d64f45a4a5e717736d0fdae20
Christopher876 <christopher876@outlook.com>
2018-08-15 11:19:26 -0400

    Debug Mode is complete!
    -As needed, add more stuff to the debug code. It exports the position and scaling of the elements chosen.
    -Start work on the level editor that uses pixels to translate to the game world.
    -Create some assets that will work with the level editor. Maybe some basic ground, lava, pipe like objects for now to get some more interesting terrain going!
    -Have to figure out a way to implement it into RandomSpawning script.
    -Follow previous changelog, "save/load"

--- Assets/Scripts/Game/Debug/Start_Debug.cs
+++ Assets/Scripts/Game/Debug/Start_Debug.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class Start_Debug : MonoBehaviour {
-
+	#region Variables
 	public GameObject player;
 	public Button Shoot_Button;
 	public Button Jump_Button;
@@ -24,6 +24,10 @@ public class Start_Debug : MonoBehaviour {
 	public InputField command_Input;
 	public GameObject plus;
 	public GameObject minus;
+	public GameObject plus_y;
+	public GameObject minus_y;
+	public Button plus_y_button;
+	public Button minus_y_button;
 	public Button plus_button;
 	public Button minus_button;
 	private Transform transform;
@@ -31,57 +35,213 @@ public class Start_Debug : MonoBehaviour {
 
 	public bool textInput = false;
 	private bool localInput = false;
+	private bool isNumber;
 
 	public float PlusOrSubtract = 1;
-
-	public void Update()
+	public int i = 1;
+	#endregion
+	public void FixedUpdate()
 	{
 		if (textInput)
 		{
 			user_Input = command_Input.text;
-			if (user_Input != "" && command_Input.isFocused && Input.GetKey(KeyCode.Return))
+			if (user_Input != "" && command_Input.isFocused && (Input.GetKey(KeyCode.Return) || Device_Config.device_type == DeviceType.Handheld))
 			{
 				Debug.Log(user_Input);
 				parseInfo();
+				command_Input.text = "";
 			}
 		}
 
 		if (localInput)
 		{
+			float num;
 			string local_input = command_Input.text;
-			if (local_input != "" && command_Input.isFocused && Input.GetKey(KeyCode.Return))
+			isNumber = float.TryParse(local_input, out num);
+			if (isNumber && (Input.GetKey(KeyCode.Return) || Device_Config.device_type == DeviceType.Handheld))
 			{
-				local_input = local_input + "f";
-				PlusOrSubtract = float.Parse(command_Input.text);
+				PlusOrSubtract = num;
+				command_Input.text = "";
+			}
+			else if (local_input == "exit" && (Input.GetKey(KeyCode.Return) || Device_Config.device_type == DeviceType.Handheld))
+			{
+				SetupExit();
+				command_Input.text = "";
 			}
 
 		}
 	}
 
+	public void SetupExit()
+	{
+		localInput = false;
+		textInput = true;
+		plus.SetActive(false);
+		minus.SetActive(false);
+		plus_y.SetActive(false);
+		minus_y.SetActive(false);
+		plus_button.onClick.RemoveAllListeners();
+		minus_button.onClick.RemoveAllListeners();
+		minus_y_button.onClick.RemoveAllListeners();
+		plus_y_button.onClick.RemoveAllListeners();
+	}
+
+	//This is where you add more keywords for the parser to use and call methods
 	public void parseInfo()
 	{
 		switch (user_Input)
 		{
+			case "save":
+				Save();
+				break;
+
 			case "shoot position":
-				textInput = false;
-				localInput = true;
-				command_Input.text = "";
-				transform = GameObject.Find("Shoot_Button").transform;
+				transform = Shoot_Button.transform;
 				ControlButton(transform);
+				SetupPosition();
 				break;
 
 			case "shoot scale":
-				textInput = false;
-				localInput = true;
-				command_Input.text = "";
-				transform = GameObject.Find("Shoot_Button").transform;
+				transform = Shoot_Button.transform;
 				ControlButton(transform);
-				plus_button.onClick.AddListener(AddScaleX);
-				minus_button.onClick.AddListener(SubtractScaleX);
+				SetupScale();
+				break;
+
+			case "gun scale":
+				transform = gun.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "gun position":
+				transform = gun.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "jump scale":
+				transform = Jump_Button.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "jump position":
+				transform = Jump_Button.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "score position":
+				transform = score.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "score scale":
+				transform = score.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "score value position":
+				transform = score_value.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "score value scale":
+				transform = score_value.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "time position":
+				transform = Time.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "time scale":
+				transform = Time.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "time value position":
+				transform = time_value.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "time value scale":
+				transform = time_value.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "health pos":
+				transform = health.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "health scale":
+				transform = health.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "rank pos":
+				transform = rank.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "rank scale":
+				transform = rank.transform;
+				ControlButton(transform);
+				SetupScale();
+				break;
+
+			case "rank value pos":
+				transform = rank.transform;
+				ControlButton(transform);
+				SetupPosition();
+				break;
+
+			case "rank value scale":
+				transform = rank.transform;
+				ControlButton(transform);
+				SetupScale();
 				break;
 		}
 	}
 
+	//This is repurposing the buttons
+	public void SetupScale()
+	{
+		textInput = false;
+		localInput = true;
+		command_Input.text = "";
+		plus_button.onClick.AddListener(AddScaleX);
+		minus_button.onClick.AddListener(SubtractScaleX);
+		plus_y_button.onClick.AddListener(AddScaleY);
+		minus_y_button.onClick.AddListener(SubtractScaleY);
+	}
+
+	//Repurposing the buttons
+	public void SetupPosition()
+	{
+		textInput = false;
+		localInput = true;
+		command_Input.text = "";
+		plus_button.onClick.AddListener(AddX);
+		minus_button.onClick.AddListener(SubtractX);
+		plus_y_button.onClick.AddListener(AddY);
+		minus_y_button.onClick.AddListener(SubtractY);
+	}
+
+	//Called to open and close the dialog and stop/reset player movement
 	public void ControlMovement (){
 
 		if (!textInput)
@@ -109,6 +269,14 @@ public class Start_Debug : MonoBehaviour {
 		command_input_activation.SetActive(false);
 		plus.SetActive(false);
 		minus.SetActive(false);
+		plus_y.SetActive(false);
+		minus_y.SetActive(false);
+
+		plus_button.onClick.RemoveAllListeners();
+		minus_button.onClick.RemoveAllListeners();
+		minus_y_button.onClick.RemoveAllListeners();
+		plus_y_button.onClick.RemoveAllListeners();
+		textInput = false;
 	}
 
 	public void ControlButton(Transform transform)
@@ -116,8 +284,11 @@ public class Start_Debug : MonoBehaviour {
 		Debug.Log("" + transform.position.x + "," + transform.position.y);
 		plus.SetActive(true);
 		minus.SetActive(true);
+		plus_y.SetActive(true);
+		minus_y.SetActive(true);
 	}
 
+	#region Position Functions
 	public void AddX()
 	{
 		Vector3 X = transform.position;
@@ -125,12 +296,46 @@ public class Start_Debug : MonoBehaviour {
 		transform.position = X;
 	}
 
-
 	public void SubtractX()
 	{
 		Vector3 X = transform.position;
 		X.x = transform.position.x - PlusOrSubtract;
 		transform.position = X;
+	}
+
+	public void AddY()
+	{
+		Vector3 Y = transform.position;
+		Y.y = transform.position.y + PlusOrSubtract;
+		//Y.x = transform.position.x - PlusOrSubtract;
+		transform.position = Y;
+		
+	}
+
+	public void SubtractY()
+	{
+		Vector3 Y = transform.position;
+		Y.y = transform.position.y - PlusOrSubtract;
+		//Y.x = transform.position.x + PlusOrSubtract;
+		transform.position = Y;
+	}
+	#endregion
+
+	#region Scale Functions
+	public void AddScaleY()
+	{
+		Vector3 Y = transform.localScale;
+		Y.y = transform.localScale.y + PlusOrSubtract;
+		//Y.x = transform.localScale.x - PlusOrSubtract;
+		transform.localScale = Y;
+	}
+
+	public void SubtractScaleY()
+	{
+		Vector3 Y = transform.localScale;
+		Y.y = transform.localScale.y - PlusOrSubtract;
+		//Y.x = transform.localScale.x + PlusOrSubtract;
+		transform.localScale = Y;
 	}
 
 	public void AddScaleX()
@@ -146,7 +351,36 @@ public class Start_Debug : MonoBehaviour {
 		X.x = transform.localScale.x - PlusOrSubtract;
 		transform.localScale = X;
 	}
+	#endregion
 
+	public void Save()
+	{
+		string destination = Application.persistentDataPath + "/debug_UI_settings.txt";
 
+		string[] lines = {"Shoot Button Position: " + Shoot_Button.transform.position.x,"," + Shoot_Button.transform.position.y,
+						"Shoot Button Scale: " + Shoot_Button.transform.localScale.x, "," + Shoot_Button.transform.localScale.y,
+						"Jump Button Position: " + Jump_Button.transform.position.x,"," + Jump_Button.transform.position.y,
+						"Jump Button Scale: " + Jump_Button.transform.localScale.x, "," + Jump_Button.transform.localScale.y,
+						"Rank Button Position: " + rank.transform.position.x,"," + rank.transform.position.y,
+						"Rank Button Scale: " + rank.transform.localScale.x, "," + rank.transform.localScale.y,
+						"Rank Value Button Position: " + rank_value.transform.position.x,"," + rank_value.transform.position.y,
+						"Rank Value Button Scale: " + rank_value.transform.localScale.x, "," + rank_value.transform.localScale.y,
+						"Time Button Position: " + Time.transform.position.x,"," + Time.transform.position.y,
+						"Time Button Scale: " + Time.transform.localScale.x, "," + Time.transform.localScale.y,
+						"Time Value Button Position: " + time_value.transform.position.x,"," + time_value.transform.position.y,
+						"Time Value Button Scale: " + time_value.transform.localScale.x, "," + time_value.transform.localScale.y,
+						"Score Button Position: " + score.transform.position.x,"," + score.transform.position.y,
+						"Score Button Scale: " + score.transform.localScale.x, "," + score.transform.localScale.y,
+						"Score Value Button Position: " + score_value.transform.position.x,"," + score_value.transform.position.y,
+						"Score Value Button Scale: " + score_value.transform.localScale.x, "," + score_value.transform.localScale.y,
+						"Health Position: " + health.transform.position.x,"," + health.transform.position.y,
+						"Health Scale: " + health.transform.localScale.x, "," + health.transform.localScale.y,
+						"Gun Button Position: " + gun.transform.position.x,"," + gun.transform.position.y,
+						"Gun Button Scale: " + gun.transform.localScale.x, "," + gun.transform.localScale.y,
+						"AmmoCount Button Position: " + ammoCount.transform.position.x,"," + ammoCount.transform.position.y,
+						"AmmoCount Button Scale: " + ammoCount.transform.localScale.x, "," + ammoCount.transform.localScale.y,};
+
+		System.IO.File.WriteAllLines(destination, lines);
+	}
 
 }
---
+++ Assets/Scripts/Game/Devices/Device_Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Device_Config : MonoBehaviour {
+
+	public static string device_model;
+	public static DeviceType device_type;
+	
+	void Start () {
+		device_model = SystemInfo.deviceModel;
+		device_type = SystemInfo.deviceType;
+	}
+	
+}
